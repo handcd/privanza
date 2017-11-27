31a0c64fc9b65d78bf236bf3f25b56dab3e7eab2
Humberto Alcocer <humbertowoody@gmail.com>
2017-11-26 20:02:29 -0600

    Finished routing Vendedor functionality

--- routes/vendedor.php
+++ routes/vendedor.php
@@ -1,32 +1,32 @@
 <?php
 
+// Dashboard
 Route::get('/dashboard', function () {
-    $users[] = Auth::user();
+    /*$users[] = Auth::user();
     $users[] = Auth::guard()->user();
-    $users[] = Auth::guard('vendedor')->user();
+    $users[] = Auth::guard('vendedor')->user();*/
 
     //dd($users);
 
     return view('vendedor.dashboard');
 })->name('dashboard');
 
-Route::get('/ordenes', function () {
-    $users[] = Auth::user();
-    $users[] = Auth::guard()->user();
-    $users[] = Auth::guard('vendedor')->user();
+// Clientes functionality
+Route::get('/clientes','Vendedor\ClientController@index');
+Route::get('/clientes/agregar', 'Vendedor\ClientController@create');
+Route::post('/clientes','Vendedor\ClientController@store');
+Route::get('/clientes/{client}','Vendedor\ClientController@show');
+Route::get('/clientes/{client}/editar','Vendedor\ClientController@edit');
+Route::put('/clientes/{client}','Vendedor\ClientController@update');
 
-    //dd($users);
+// Orders
+Route::get('/ordenes','Vendedor\OrderController@index');
+Route::get('/ordenes/agregar', 'Vendedor\OrderController@create');
+Route::post('/ordenes','Vendedor\OrderController@store');
+Route::get('/ordenes/{order}','Vendedor\OrderController@show');
+Route::get('/ordenes/{order}/editar','Vendedor\OrderController@edit');
+Route::put('/ordenes/{order}','Vendedor\OrderController@update');
 
-    return view('vendedor.orders');
-})->name('ordenes');
-
-Route::get('/profile', function () {
-    $users[] = Auth::user();
-    $users[] = Auth::guard()->user();
-    $users[] = Auth::guard('vendedor')->user();
-
-    //dd($users);
-
-    return view('vendedor.profile');
-})->name('profile');
-
+// Profile
+Route::get('/perfil','Vendedor\ProfileController@index');
+Route::get('/perfil/solicitarCambio', 'Vendedor\ProfileController@askDataChange');
