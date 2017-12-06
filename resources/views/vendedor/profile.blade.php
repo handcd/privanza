184bd262bb3c5c6f150f5f068d08085136bc46fa
Humberto Alcocer <humbertowoody@gmail.com>
2017-12-05 19:04:26 -0600

    Added fields to profile view for Vendedor

--- resources/views/vendedor/profile.blade.php
+++ resources/views/vendedor/profile.blade.php
@@ -18,23 +18,23 @@
         @endif
         <div class="card">
             <div class="card-header" data-background-color="purple">
-                <h4 class="title">{{ $vendedor->name }}</h4>
+                <h4 class="title"> Tus datos {{ $vendedor->name }}</h4>
                 <p class="category">Aquí está tu información, con ella te contactaremos para cualquier situación. Corrobora que sea correcta.</p>
             </div>
             <div class="card-content">
                 <div class="row">
                     <div class="col-md-12">
-                        <h2>Datos de contacto.</h2>
+                        <h4>Información General</h4>
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-md-6">
+                    <div class="col-md-4">
                         <label class="text-primary">Nombre completo:</label>
-                        <p>{{ $vendedor->name }}</p>
+                        <p>{{ $vendedor->name.' '.$vendedor->lastname }}</p>
                     </div>
-                    <div class="col-md-6">
+                    <div class="col-md-8">
                         <label class="text-primary">Domicilio</label>
-                        <p>{{ $vendedor->address }}</p>
+                        <p>{{ $vendedor->address_home }}</p>
                     </div>
                 </div>
                 <div class="row">
@@ -45,6 +45,35 @@
                     <div class="col-md-6">
                         <label class="text-primary">Teléfono de contacto</label>
                         <p><a href="tel:{{ $vendedor->phone }}">{{ $vendedor->phone }}</a></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <label class="text-primary">Fecha de Cumpleaños</label>
+                        <p>{{ $vendedor->birthday }}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h4>Información de Facturación</h4>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label class="text-primary">R.F.C.</label>
+                        <p>{{ $vendedor->rfc ? $vendedor->rfc : 'No ha sido cargado al sistema' }}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="text-primary">Últimos Dígitos de la Cuenta</label>
+                        <p>{{ $vendedor->account_digits ? $vendedor->account_digits : 'No ha sido cargado al sistema' }}</p>
+                    </div>
+                    <div class="col-md-4">
+                        <label class="text-primary">Banco</label>
+                        <p>{{ $vendedor->bank ? $vendedor->bank : 'No ha sido cargado al sistema' }}</p>
+                    </div>
+                    <div class="col-md-6">
+                        <label class="text-primary">Concepto de Facturación</label>
+                        <p>{{ $vendedor->concept ? $vendedor->concept : 'No ha sido cargado al sistema' }}</p>
                     </div>
                 </div>
                 <a href="{{ url('/vendedor/perfil/solicitarCambio') }}" class="btn btn-primary pull-right">Solicitar cambio de información</a>
