2d30c60eae22468b6562fd3bffbc8887e026207c
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-12 15:29:08 -0600

    Fixed birthday faker to match real dates

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -11,7 +11,7 @@ $factory->define(App\Client::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'address_visit' => $faker->streetAddress,
         'address_delivery' => $faker->streetAddress,
-        'birthday' => $faker->dateTimeBetween($startDate = '-30 years', $endDate = 'now', $timezone = null),
+        'birthday' => $faker->dateTimeBetween($startDate = '-80 years', $endDate = '-15 years', $timezone = null),
         'notes' => $faker->text($maxNbChars = 200),
         // Datos Facturación
         'address_legal' => $faker->address,
