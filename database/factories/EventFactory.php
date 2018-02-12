3b222c1d107790cc3a96e5879d1a3cb2fcded167
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-12 17:19:52 -0600

    Fixed vendedor_id on the EventFactory to get a random item from collection

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,9 +3,8 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function (Faker $faker) {
-	$vendedor = $faker->numberBetween($min = 1, $max = 20);
     return [
-        'vendedor_id' => $vendedor,
+        'vendedor_id' => App\Vendedor::random()->id,
         'client_id' => App\Vendedor::find($vendedor)->clients->random()->id,
         'fechahora' => $faker->dateTimeBetween($startDate = '-3 months', $endDate = '+1 month'),
     ];
