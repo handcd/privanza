b7da71c19be5b2faa126fb931fef5e24eb2c2073
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-12 17:24:15 -0600

    Added hackity hack to get optimization on the password generation for Validadors

--- database/factories/ValidadorFactory.php
+++ database/factories/ValidadorFactory.php
@@ -3,11 +3,13 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Validador::class, function (Faker $faker) {
+	static $password;
+
     return [
         'name' => $faker->name,
         'lastname' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
-        'password' => bcrypt('validador123'),
+        'password' => $password ?: $password = bcrypt('validador123'),
         'birthday' => $faker->dateTimeThisCentury,
         'job_position' => $faker->jobTitle,
         'enabled' => 1,
