3f49752da5659988bb2b2ae7ad41e483a95745b5
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-11 23:46:34 -0600

    Reviewed discrepations from input on pants

--- database/migrations/2017_12_26_232437_create_pants_table.php
+++ database/migrations/2017_12_26_232437_create_pants_table.php
@@ -16,24 +16,14 @@ class CreatePantsTable extends Migration
         Schema::create('pants', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('order_id');
-            $table->boolean('pretina_bies');
-            $table->string('pretina_bies_color')->nullable();
-            $table->boolean('pretina_pin_point');
-            $table->string('pretina_pin_point_color')->nullable();
-            $table->boolean('pretina_flexon');
-            $table->boolean('pretina_snutex');
-            $table->boolean('pretina_snutex_bies');
-            $table->boolean('pretina_snutex_pin_point');
-            $table->boolean('pretina_snutex_sola');
-            $table->integer('no_bolsas_delanteras');
-            $table->integer('no_bolsas_traseras');
-            $table->integer('tipo_vivo'); // sencillo/doble
-            $table->integer('tipo_cerrado'); // ojal y botón/traba y botón
-            $table->boolean('ribete');
-            $table->string('ribete_color')->nullable();
-            $table->boolean('medioforno');
-            $table->boolean('dobladillo_personalizado');
-            $table->integer('tipo_dobladillo_personalizado')->nullable(); // normal/valenciana española
+
+            $table->boolean('pase'); // Con pase, Sin Pase
+            $table->integer('pliegues'); // 0-2
+            $table->integer('bolsas_traseras');
+            $table->integer('tipo_vivo'); // 1 Vivo Doble con Ojal, 2 Vivo Sencillo con Ojal
+            $table->string('color_ojalera');
+            $table->string('color_medio_forro');
+            $table->integer('dobladillo'); // 1 normal, 2 valenciana
             $table->timestamps();
         });
     }
