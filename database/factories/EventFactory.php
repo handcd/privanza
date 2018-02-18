92194018e9654680b812097731b0714a46c93583
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-17 19:53:39 -0600

    Fixed wrong field name on the EventSeeder

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -3,9 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Event::class, function (Faker $faker) {
+	$vendedor = App\Vendedor::all()->random();
     return [
-        'vendedor_id' => App\Vendedor::random()->id,
-        'client_id' => App\Vendedor::find($vendedor)->clients->random()->id,
+        'vendedor_id' => $vendedor->id,
+        'client_id' => $vendedor->clients->random()->id,
         'fechahora' => $faker->dateTimeBetween($startDate = '-3 months', $endDate = '+1 month'),
     ];
 });
