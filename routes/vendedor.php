a93d4c6fcb36a0149a462cbebbbbc0ce3a0764ce
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-17 19:52:36 -0600

    Fixed routes in order to match new controller changes

--- routes/vendedor.php
+++ routes/vendedor.php
@@ -4,33 +4,33 @@
 Route::get('/dashboard','DashboardController@vendedorDash')->name('dashboard');
 
 // Clientes
-Route::get('/clientes','ClientController@indexVendedor');
-Route::get('/clientes/agregar', 'ClientController@create');
-Route::post('/clientes','ClientController@store');
-Route::get('/clientes/{client}','ClientController@show');
-Route::get('/clientes/{client}/editar','ClientController@edit');
-Route::put('/clientes/{client}','ClientController@update');
+Route::get('/clientes','ClientController@indexForVendedor');
+Route::get('/clientes/agregar', 'ClientController@createForVendedor');
+Route::post('/clientes','ClientController@storeForVendedor');
+Route::get('/clientes/{client}','ClientController@showForVendedor');
+Route::get('/clientes/{client}/editar','ClientController@editForVendedor');
+Route::put('/clientes/{client}','ClientController@updateForVendedor');
 
 // Orders
-Route::get('/ordenes','OrderController@indexVendedor');
-Route::get('/ordenes/agregar', 'OrderController@create');
-Route::post('/ordenes','OrderController@store');
-Route::get('/ordenes/{order}','OrderController@show');
-Route::get('/ordenes/{order}/editar','OrderController@edit');
-Route::put('/ordenes/{order}','OrderController@update');
-Route::get('/ordenes/{order}/pdf','OrderController@orderpdf');
+Route::get('/ordenes','OrderController@indexForVendedor');
+Route::get('/ordenes/agregar', 'OrderController@createForVendedor');
+Route::post('/ordenes','OrderController@storeForVendedor');
+Route::get('/ordenes/{order}','OrderController@showForVendedor');
+Route::get('/ordenes/{order}/editar','OrderController@editForVendedor');
+Route::put('/ordenes/{order}','OrderController@updateForVendedor');
+Route::get('/ordenes/{order}/pdf','OrderController@orderpdfForVendedor');
 
 // Events
-Route::get('/citas','EventController@index');
-Route::get('/citas/agregar','EventController@create');
-Route::post('/citas','EventController@store');
-Route::get('/citas/{cita}','EventController@show');
-Route::get('/citas/{cita}/editar','EventController@edit');
-Route::put('/citas/{cita}','EventController@update');
+Route::get('/citas','EventController@indexForVendedor');
+Route::get('/citas/agregar','EventController@createForVendedor');
+Route::post('/citas','EventController@storeForVendedor');
+Route::get('/citas/{cita}','EventController@showForVendedor');
+Route::get('/citas/{cita}/editar','EventController@editForVendedor');
+Route::put('/citas/{cita}','EventController@updateForVendedor');
 
 // Profile
-Route::get('/perfil','ProfileController@index');
-Route::get('/perfil/solicitarCambio', 'ProfileController@askDataChange');
+Route::get('/perfil','ProfileController@perfilVendedor');
+Route::get('/perfil/solicitarCambio', 'ProfileController@dataChangeVendedor');
 
 Route::get('/pruebamail', function() {
     return view('mails.action',[
