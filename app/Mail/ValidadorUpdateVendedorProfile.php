ee4b01882c05047dbf8e82001ad10776704155c0
Humberto Alcocer <humbertowoody@gmail.com>
2018-02-28 11:11:25 -0600

    Deleted special chars HTML that were not rendering correctly on email

--- app/Mail/ValidadorUpdateVendedorProfile.php
+++ app/Mail/ValidadorUpdateVendedorProfile.php
@@ -33,7 +33,7 @@ class ValidadorUpdateVendedorProfile extends Mailable
         // Variables for the message
         $asunto = 'Cambio de Información de Vendedor';
         $titulo = $this->vendedor->name.' ha solicitado una revisión de información';
-        $cuerpo = $this->vendedor->name.' '.$this->vendedor->lastname.' ha solicitado una revisión de su información. Para ponerse en contacto con él te proporcionamos su información de contacto y posteriormente podrás hacer los cambios pertinentes haciendo click en el botón.<br>br>Correo: '.$this->vendedor->email.'<br> Teléfono: '.$this->vendedor->phone;
+        $cuerpo = $this->vendedor->name.' '.$this->vendedor->lastname.' ha solicitado una revisión de su información. Para ponerse en contacto con él te proporcionamos su información de contacto y posteriormente podrás hacer los cambios pertinentes haciendo click en el botón. Correo: '.$this->vendedor->email.', Teléfono: '.$this->vendedor->phone;
         $mensajeAccion = 'Ver Vendedor';
         $urlAccion = '/validador/vendedores/'.$this->vendedor->id;
 
