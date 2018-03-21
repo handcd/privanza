230dd2849cb45fb209f0bd36283869708da5b28b
Humberto Alcocer <humbertowoody@gmail.com>
2018-03-21 16:24:10 -0600

    Updated the routes to match new architecture

--- routes/validador.php
+++ routes/validador.php
@@ -11,32 +11,32 @@ Route::get('/dashboard', function () {
 })->name('dashboard');
 
 // Clientes functionality
-Route::get('/clientes','Validador\ClientController@index');
-Route::get('/clientes/agregar', 'Validador\ClientController@create');
-Route::post('/clientes','Validador\ClientController@store');
-Route::get('/clientes/{client}','Validador\ClientController@show');
-Route::get('/clientes/{client}/editar','Validador\ClientController@edit');
-Route::put('/clientes/{client}','Validador\ClientController@update');
+Route::get('/clientes','ClientController@index');
+Route::get('/clientes/agregar', 'ClientController@create');
+Route::post('/clientes','ClientController@store');
+Route::get('/clientes/{client}','ClientController@show');
+Route::get('/clientes/{client}/editar','ClientController@edit');
+Route::put('/clientes/{client}','ClientController@update');
 
 // Orders
-Route::get('/ordenes','Validador\OrderController@index');
-Route::get('/ordenes/agregar', 'Validador\OrderController@create');
-Route::post('/ordenes','Validador\OrderController@store');
-Route::get('/ordenes/{order}','Validador\OrderController@show');
-Route::get('/ordenes/{order}/editar','Validador\OrderController@edit');
-Route::put('/ordenes/{order}','Validador\OrderController@update');
+Route::get('/ordenes','OrderController@index');
+Route::get('/ordenes/agregar', 'OrderController@create');
+Route::post('/ordenes','OrderController@store');
+Route::get('/ordenes/{order}','OrderController@show');
+Route::get('/ordenes/{order}/editar','OrderController@edit');
+Route::put('/ordenes/{order}','OrderController@update');
 Route::get('/ordenes/{order}/aprobar');
 
 // Vendedores
-Route::get('/vendedores','Validador\VendedorController@index');
-Route::get('/vendedores/agregar','Validador\VendedorController@create');
-Route::post('/vendedores','Validador\VendedorController@store');
-Route::get('/vendedores/{vendedor}','Validador\VendedorController@show');
-Route::get('/vendedores/{vendedor}/editar','Validador\VendedorController@edit');
-Route::put('/vendedores/{vendedor}','Validador\VendedorController@update');
-Route::get('/vendedores/{vendedor}/activar','Validador\VendedorController@activar');
-Route::get('/vendedores/{vendedor}/desactivar','Validador\VendedorController@desactivar');
+Route::get('/vendedores','VendedorController@index');
+Route::get('/vendedores/agregar','VendedorController@create');
+Route::post('/vendedores','VendedorController@store');
+Route::get('/vendedores/{vendedor}','VendedorController@show');
+Route::get('/vendedores/{vendedor}/editar','VendedorController@edit');
+Route::put('/vendedores/{vendedor}','VendedorController@update');
+Route::get('/vendedores/{vendedor}/activar','VendedorController@activar');
+Route::get('/vendedores/{vendedor}/desactivar','VendedorController@desactivar');
 
 // Profile
-Route::get('/perfil','Validador\ProfileController@index');
-Route::get('/perfil/solicitarCambio', 'Validador\ProfileController@askDataChange');
+Route::get('/perfil','ProfileController@perfilValidador');
+Route::get('/perfil/solicitarCambio', 'ProfileController@dataChangeValidador');
