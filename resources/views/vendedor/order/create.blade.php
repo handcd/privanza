fef553a6f505cf68c32d995a56038fae827b26cb
Pablo Contreras <pablo_contreras_1997@outlook.com>
2018-03-22 11:51:50 -0600

    Se cambió el texto en los el texto de las bolsas internas

--- resources/views/vendedor/order/create.blade.php
+++ resources/views/vendedor/order/create.blade.php
@@ -860,7 +860,7 @@
 							<div class="row">
 								<div class="col-md-5 col-md-offset-1">
 									<div class="row">
-										<h5 class="text-center">Vivos en Bolsas Internas</h5>
+										<h5 class="text-center">Forro de Vivos en Bolsas Internas</h5>
 									</div>
 									<div class="row col-xs-offset-1">
 										<div class="checkbox">
