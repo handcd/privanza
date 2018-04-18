36efe3eed11f3de472c20f919c6b66d1193779de
Humberto Alcocer <humbertowoody@gmail.com>
2018-04-18 16:13:54 -0500

    Added new functions for index and show Validadores for Admin

--- app/Http/Controllers/ValidadorController.php
+++ app/Http/Controllers/ValidadorController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Auth;
+use Notification;
+
+// Models
+use App\Vendedor;
+use App\Admin;
+use App\Validador;
 
 class ValidadorController extends Controller
 {
-    //
+	/**
+	 * Display the home view for the CRUD
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+    public function index()
+    {
+    	$validadores = Validador::paginate(30);
+    	return view('admin.validador.home',compact('validadores'));
+    }
+
+
+    /**
+     * Display the specified resource
+     *
+     * @param $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    	$validador = Validador::find($id);
+
+    	if (!$validador) {
+    		$request->session()->flash('danger', 'El Validador que deseas visualizar no se encuentra registrado en el sistema, verifica la información e inténtalo de nuevo.');
+    		return redirect('/admin/validadores');
+    	}
+
+    	return view('admin.validador.show', compact('validador'));
+    }
+
+
 }
