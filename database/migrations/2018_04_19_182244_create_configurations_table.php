01874da0d3952b6b83a9f04191c36a3af22e4c1c
Humberto Alcocer <humbertowoody@gmail.com>
2018-04-22 21:14:16 -0500

    Added main table for configuration

--- database/migrations/2018_04_19_182244_create_configurations_table.php
+++ database/migrations/2018_04_19_182244_create_configurations_table.php
@@ -14,6 +14,7 @@ class CreateConfigurationsTable extends Migration
     public function up()
     {
         Schema::create('configurations', function (Blueprint $table) {
+            // Tiempos de aviso entre acciones
             $table->integer('horas_aviso_no_aprobada')->default(22);
             $table->integer('horas_aviso_aprobada')->default(22);
             $table->integer('horas_aviso_produccion')->default(22);
@@ -22,6 +23,44 @@ class CreateConfigurationsTable extends Migration
             $table->integer('horas_aviso_produccion_plancha')->default(22);
             $table->integer('horas_aviso_produccion_revision')->default(22);
             $table->integer('horas_aviso_pickup')->default(22);
+
+            // Activar/Desactivar Notificaciones
+            // Admin
+            $table->boolean('notificar_admin_nueva_orden')->default(1);
+            $table->boolean('notificar_admin_cambio_orden')->default(1);
+            $table->boolean('notificar_admin_status_orden')->default(1);
+            $table->boolean('notificar_admin_nuevo_vendedor')->default(1);
+            $table->boolean('notificar_admin_cambio_vendedor')->default(1);
+            $table->boolean('notificar_admin_nuevo_cliente')->default(1);
+            $table->boolean('notificar_admin_cambio_cliente')->default(1);
+            $table->boolean('notificar_admin_nuevo_ajuste')->default(1);
+            $table->boolean('notificar_admin_cambio_ajuste')->default(1);
+            $table->boolean('notificar_admin_nueva_cita')->default(1);
+            $table->boolean('notificar_admin_cambio_cita')->default(1);
+            // Validador
+            $table->boolean('notificar_validador_desactivado')->default(0);
+            $table->boolean('notificar_validador_nueva_orden')->default(1);
+            $table->boolean('notificar_validador_cambio_orden')->default(1);
+            $table->boolean('notificar_validador_status_orden')->default(1);
+            $table->boolean('notificar_validador_nuevo_vendedor')->default(1);
+            $table->boolean('notificar_validador_cambio_vendedor')->default(1);
+            $table->boolean('notificar_validador_nuevo_cliente')->default(1);
+            $table->boolean('notificar_validador_cambio_cliente')->default(1);
+            $table->boolean('notificar_validador_nuevo_ajuste')->default(1);
+            $table->boolean('notificar_validador_cambio_ajuste')->default(1);
+            $table->boolean('notificar_validador_nueva_cita')->default(1);
+            $table->boolean('notificar_validador_cambio_cita')->default(1);
+            // Vendedor
+            $table->boolean('notificar_vendedor_desactivado')->default(0);
+            $table->boolean('notificar_vendedor_nueva_orden')->default(1);
+            $table->boolean('notificar_vendedor_cambio_orden')->default(1);
+            $table->boolean('notificar_vendedor_status_orden')->default(1);
+            $table->boolean('notificar_vendedor_nuevo_cliente')->default(1);
+            $table->boolean('notificar_vendedor_cambio_cliente')->default(1);
+            $table->boolean('notificar_vendedor_nueva_cita')->default(1);
+            $table->boolean('notificar_vendedor_cambio_cita')->default(1);
+
+
             $table->timestamps();
         });
     }
