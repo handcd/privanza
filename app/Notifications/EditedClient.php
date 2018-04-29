230614790a08dbc446fd253836f0e23debcfb12c
Humberto Alcocer <humbertowoody@gmail.com>
2018-04-29 16:31:29 -0500

    Finished notifications for Client actions

--- app/Notifications/EditedClient.php
+++ app/Notifications/EditedClient.php
@@ -7,18 +7,27 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class EditedClient extends Notification
+class EditedClient extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * The User and the Client
+     * @var $user
+     * @var \App\Client $client
+     */
+    protected $user;
+    protected $client;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user, $client)
     {
-        //
+        $this->user = $user;
+        $this->client = $client;
     }
 
     /**
@@ -40,22 +49,26 @@ class EditedClient extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        if ($this->user->isAdmin()) {
+            return (new MailMessage)
+                        ->subject('Se ha editado la información de un Cliente')
+                        ->line('La información del cliente '.$this->client->name.' '.$this->client->lastaname.' con ID #'.$this->client->id.' ha sido modificada.')
+                        ->line('Para ver la información actualizada haz click en el siguiente botón:')
+                        ->action('Ver Cliente',url('/admin/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el Sistema!');
+        } elseif ($this->user->isValidador()) {
+            return (new MailMessage)
+                        ->subject('Se ha editado la información de un Cliente')
+                        ->line('La información del cliente '.$this->client->name.' '.$this->client->lastaname.' con ID #'.$this->client->id.' ha sido modificada.')
+                        ->line('Para ver la información actualizada haz click en el siguiente botón:')
+                        ->action('Ver Cliente',url('/validador/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el Sistema!');
+        } else {
+            return (new MailMessage)
+                        ->subject('Se modificó la información de un Cliente')
+                        ->line('Se ha modificado la información de tu cliente '.$this->client->name.' '.$this->client->lastname.'. Para revisar este y  otros cambios haz click en el siguiente botón:')
+                        ->action('Ver Cliente', url('/vendedor/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el sistema!');
+        }
     }
 }
--- app/Notifications/NewClient.php
+++ app/Notifications/NewClient.php
@@ -7,18 +7,27 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class NewClient extends Notification
+class NewClient extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * The Client and the User to be notified
+     * @var \App\Client $client
+     * @var $user
+     */
+    protected $client;
+    protected $user;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user, $client)
     {
-        //
+        $this->client = $client;
+        $this->user = $user;
     }
 
     /**
@@ -40,22 +49,30 @@ class NewClient extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        if ($this->user->isAdmin()) {
+            return (new MailMessage)
+                        ->subject('Nuevo Cliente añadido al Sistema')
+                        ->line('Un nuevo cliente ha sido añadido al sistema.')
+                        ->line('Nombre: '.$this->client->name.' '.$this->client->lastname)
+                        ->line(' Para revisar su información, haz click en el siguiente botón:')
+                        ->action('Revisar Cliente',url('/admin/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el sistema!');
+        } elseif ($this->user->isValidador()) {
+            return (new MailMessage)
+                        ->subject('Nuevo Cliente añadido al Sistema')
+                        ->line('Un nuevo cliente ha sido añadido al sistema.')
+                        ->line('Nombre: '.$this->client->name.' '.$this->client->lastname)
+                        ->line(' Para revisar su información, haz click en el siguiente botón:')
+                        ->action('Revisar Cliente',url('/validador/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el sistema!');
+        } else {
+            return (new MailMessage)
+                        ->subject('Se te ha añadido un nuevo Cliente')
+                        ->line('Un nuevo cliente ha sido añadido para que lo atiendas.')
+                        ->line('Nombre: '.$this->client->name.' '.$this->client->lastname)
+                        ->line('Para revisar los datos completos del cliente, así como añadir una cita o un pedido, puedes hacerlo haciendo click en el siguiente botón:')
+                        ->action('Revisar Cliente',url('/vendedor/clientes',$this->client->id))
+                        ->line('¡Gracias por usar el sistema!');
+        }
     }
 }
