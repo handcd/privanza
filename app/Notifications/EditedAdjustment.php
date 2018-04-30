7fbb25088aeb6176d3a2ac0608e6377438123e8c
Humberto Alcocer <humbertowoody@gmail.com>
2018-04-29 19:20:49 -0500

    Finished Adjustments' Notifications

--- app/Notifications/EditedAdjustment.php
+++ app/Notifications/EditedAdjustment.php
@@ -7,18 +7,27 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class EditedAdjustment extends Notification
+class EditedAdjustment extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * The User and the AdjustmentsOrder
+     * @var $user
+     * @var \App\AdjustmentsOrder $adjustment
+     */
+    protected $user;
+    protected $adjustment;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user, $adjustment)
     {
-        //
+        $this->user = $user;
+        $this->adjustment = $adjustment;
     }
 
     /**
@@ -40,22 +49,18 @@ class EditedAdjustment extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        if ($this->user->isAdmin()) {
+            return (new MailMessage)
+                        ->subject('Se ha actualizado una Orden de Ajustes')
+                        ->line('Se ha modificado la información de la Orden de Ajustes con ID #'.$this->adjustment->id.'. Para revisar más detalles haz click en el siguiente botón:')
+                        ->action('Revisar Orden de Ajustes',url('/admin/ajustes',$this->adjustment->id))
+                        ->line('¡Gracias por usar el sistema!');
+        } else {
+            return (new MailMessage)
+                        ->subject('Se ha actualizado una Orden de Ajustes')
+                        ->line('Se ha modificado la información de la Orden de Ajustes con ID #'.$this->adjustment->id.'. Para revisar más detalles haz click en el siguiente botón:')
+                        ->action('Revisar Orden de Ajustes',url('/validador/ajustes',$this->adjustment->id))
+                        ->line('¡Gracias por usar el sistema!');
+        }
     }
 }
--- app/Notifications/NewAdjustment.php
+++ app/Notifications/NewAdjustment.php
@@ -7,18 +7,27 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class NewAdjustment extends Notification
+class NewAdjustment extends Notification implements ShouldQueue
 {
     use Queueable;
+
+    /**
+     * The User and the AdjustmentsOrder
+     * @var $user
+     * @var \App\AdjustmentsOrder $adjustment
+     */
+    protected $user;
+    protected $adjustment;
 
     /**
      * Create a new notification instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($user, $adjustment)
     {
-        //
+        $this->user = $user;
+        $this->adjustment = $adjustment;
     }
 
     /**
@@ -40,22 +49,18 @@ class NewAdjustment extends Notification
      */
     public function toMail($notifiable)
     {
-        return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
-
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
-    public function toArray($notifiable)
-    {
-        return [
-            //
-        ];
+        if ($this->user->isAdmin()) {
+            return (new MailMessage)
+                        ->subject('Nueva Orden de Ajustes en Privanza')
+                        ->line('Se ha añadido una nueva orden de ajustes en el sistema. Puedes revisarla haciendo click en el siguiente botón:')
+                        ->action('Revisar Orden de Ajsutes',url('/admin/ajustes',$this->adjustment->id))
+                        ->line('¡Gracias por usar el sistema!');
+        } else {
+            return (new MailMessage)
+                        ->subject('Nueva Orden de Ajustes en Privanza')
+                        ->line('Se ha añadido una nueva orden de ajustes en el sistema. Puedes revisarla haciendo click en el siguiente botón:')
+                        ->action('Revisar Orden de Ajsutes',url('/ajustes/ajustes',$this->adjustment->id))
+                        ->line('¡Gracias por usar el sistema!');
+        }
     }
 }
